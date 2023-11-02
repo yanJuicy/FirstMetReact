c45d4dc8f6ea33eb99540efeb2b0e4dfb657c3ad
yanJuicy <hanjub3@gmail.com>
2023-11-02 15:13:48 +0900

    Hooks

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
 import Library from "./chapter_03/Library";
 import Clock from "./chapter_04/Clock";
 import CommentList from "./chapter_05/CommentList";
 import NotificationList from "./chapter_06/NotificationList";
+import Accommodtae from "./chapter_07/Accommodate";
 
 ReactDOM.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodtae />
   </React.StrictMode>,
   document.getElementById("root")
 );
