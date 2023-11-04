9fe7a1a08c7584865569fc831807f68a021af750
yanJuicy <hanjub3@gmail.com>
2023-11-04 09:09:00 +0900

    List and Keys

---
+++ src/chapter_10/AttendanceBook.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const students = [
+  {
+    id: 1,
+    name: "Inje",
+  },
+  {
+    id: 2,
+    name: "Steve",
+  },
+  {
+    id: 3,
+    name: "Bill",
+  },
+  {
+    id: 4,
+    name: "Jeff",
+  },
+];
+
+function AttendanceBook(props) {
+  return (
+    <div>
+      <ul>
+        {students.map((student) => {
+          return <li key={student.id}>{student.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default AttendanceBook;
--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ import NotificationList from "./chapter_06/NotificationList";
 import Accommodtae from "./chapter_07/Accommodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <AttendanceBook />
   </React.StrictMode>,
   document.getElementById("root")
 );
